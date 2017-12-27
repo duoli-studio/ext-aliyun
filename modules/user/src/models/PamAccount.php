0c9f45ae73e1457d19ae7fc1f23dd1823bbcbbca
Duoli <zhaody901@126.com>
2017-12-27 18:15:48 +0800

    优化模型

--- modules/user/src/models/PamAccount.php
+++ modules/user/src/models/PamAccount.php
@@ -1,25 +1,24 @@
-<?php namespace User\models;
+<?php namespace User\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * @property-read \App\Models\PamAccount $pam
- * @property integer                     $id                 id
- * @property string                      $mobile             手机号
- * @property string                      $username           用户名称
- * @property string                      $password           用户密码
- * @property datetime                    $logined_at         注册时间
+ * @property int    $id                 id
+ * @property string $mobile             手机号
+ * @property string $username           用户名称
+ * @property string $password           用户密码
+ * @property Carbon $logined_at         注册时间
  */
 class PamAccount extends Model
 {
-	protected $table      = 'pam_account';
+	protected $table = 'pam_account';
 
-	protected $primaryKey = 'id';
+	protected $dates = [
+		'logined_at',
+	];
 
-	public    $timestamps = false;
-
-	protected $fillable   = [
-		'id',
+	protected $fillable = [
 		'mobile',
 		'username',
 		'password',
