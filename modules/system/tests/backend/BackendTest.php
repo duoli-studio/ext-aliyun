06821843af7ed05506abacbc8e63723bb9776d97
Duoli <zhaody901@126.com>
2018-01-03 10:52:32 +0800

    update  app.min

--- modules/system/tests/backend/BackendTest.php
+++ modules/system/tests/backend/BackendTest.php
@@ -2,11 +2,11 @@
 
 
 use Poppy\Framework\Application\TestCase;
-use System\Backend\_repositories\NavigationRepository;
+use System\Backend\Repositories\NavigationRepository;
+
 
 class BackendTest extends TestCase
 {
-
 
 	public function testMenu()
 	{
