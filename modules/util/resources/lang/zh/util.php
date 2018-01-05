23e0f6635134aa4a17124e046a56eaa905399f37
Duoli <zhaody901@126.com>
2018-01-05 17:38:40 +0800

    util query

---
+++ modules/util/resources/lang/zh/util.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+	'graphql' => [
+		'send_captcha_type_desc'    => '发送验证码类型',
+		'send_captcha_queries_desc' => '发送验证码',
+		'value_register_desc'       => '注册发送验证码',
+	],
+	'db' => [
+		'passport'    => '通行证',
+	],
+];
