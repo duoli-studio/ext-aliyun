3231931111220bd1e1f65002218b8f7a13fcad76
zhangnianwen <zhangnianwenmail@163.com>
2018-01-06 10:21:11 +0800

    util验证码修改

--- modules/util/resources/lang/zh/util.php
+++ modules/util/resources/lang/zh/util.php
@@ -6,6 +6,8 @@ return [
 		'send_captcha_queries_desc' => '发送验证码',
 		'send_captcha_success'      => '成功发送验证码',
 		'value_register_desc'       => '注册发送验证码',
+		'value_find_password_desc'  => '找回密码发送验证码',
+		'value_order_desc'          => '订单操作发送验证码',
 	],
 	'db'      => [
 		'passport' => '通行证',
